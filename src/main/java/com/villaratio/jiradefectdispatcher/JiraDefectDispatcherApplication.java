e27e481bcbdc261d7f2260cca960ad5a006f4fee
Pieter-Jan Savat <info@villaratio.com>
2018-04-28 21:36:23 +0200

    Initial commit

---
+++ src/main/java/com/villaratio/jiradefectdispatcher/JiraDefectDispatcherApplication.java
@@ -0,0 +1,12 @@
+package com.villaratio.jiradefectdispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraDefectDispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraDefectDispatcherApplication.class, args);
+	}
+}
